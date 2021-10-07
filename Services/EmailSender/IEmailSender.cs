152e70b2c6a496b92a9cea18a038dd96cc1956e5
dm1triykr <dimonk601@gmail.com>
2021-10-07 14:38:30 +0300

    Web Technology

---
+++ Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.EmailSender
+{
+    interface IEmailSender
+    {
+        Task SendMessage();
+    }
+}
---
+++ lab1/Models/Home/IndexVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab1.Models.Home
+{
+    public class IndexVM
+    {
+        public string MyFullName { get; set; }
+    }
+}

5b053bd1fb951af65963602340ad4ffe6127a88d
dm1triykr <dimonk601@gmail.com>
2021-10-25 22:31:38 +0300

    Web-Technologies-KHNU
    Web-Technologies-KHNU

--- Labs 1-2/lab1/Controllers/HomeController.cs
+++ Labs 1-2/lab1/Controllers/HomeController.cs
@@ -33,16 +33,12 @@ namespace lab1.Controllers
         {
             //var currentUrl = HttpContext.Request.GetEncodedUrl();
             var currentUrl = _httpContextAccessor.HttpContext.Request.GetEncodedUrl();
-            string host = Dns.GetHostName();
-            IPHostEntry ipE = Dns.GetHostEntry(host);
-            IPAddress[] IpA = ipE.AddressList;
-            for (int i = 0; i < IpA.Length; i++)
-            {
-                IpA.ToString();
-            }
+            // Получение имени компьютера.
+            String host = Dns.GetHostName();
+            // Получение ip-адреса.
+            IPAddress ip = Dns.GetHostByName(host).AddressList[0];
 
-
-            _logger.LogInformation($"Url: {currentUrl}, Time: {DateTime.Now}, IP Address: {}");
+            _logger.LogInformation($"Url: {currentUrl}, Time: {DateTime.Now}, IP Address: {ip}");
             return View();
         }
 

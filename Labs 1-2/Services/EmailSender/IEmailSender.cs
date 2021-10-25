3aa04d46b7e6433f9c38ff9e2842387ad9ddf5f7
dm1triykr <dimonk601@gmail.com>
2021-10-25 16:19:21 +0300

    Web-Technologies-KHNU
    Web-Technologies-KHNU

--- Labs 1-2/Services/EmailSender/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Services.EmailSender
-{
-    public interface IEmailSender
-    {
-        Task SendMessage(string emailTo, string messageBody, string subject);
-    }
-}

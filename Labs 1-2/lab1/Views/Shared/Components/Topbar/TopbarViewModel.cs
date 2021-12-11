9c226ef3c7fba1f67626b1e110bfc8d422cc346b
dm1triykr <dimonk601@gmail.com>
2021-12-11 20:08:52 +0200

    Added TopbarComponent

---
+++ Labs 1-2/lab1/Views/Shared/Components/Topbar/TopbarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab1.Views.Shared.Components.Topbar
+{
+    public class TopbarViewModel
+    {
+        public bool IsAdminUser { get; set; }
+    }
+}
